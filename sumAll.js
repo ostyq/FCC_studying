3420201feedf019b9a04d3bfa065de1270271335
Veronika Tolpeeva <osty1n@gmail.com>
2018-02-21 22:08:33 +0300

    add solution to sumAll algorithm

---
+++ sumAll.js
@@ -0,0 +1,13 @@
+function sumAll(arr) {
+  const start = Math.min(...arr);
+  const end = Math.max(...arr);
+//   const current = 0;
+  for (let i = start; i < end - 1; i += 1) {
+    arr.push(i + 1);
+  }
+  return arr.reduce((prev, curr) => prev + curr);
+}
+
+
+console.log(sumAll([1, 4]));
+
