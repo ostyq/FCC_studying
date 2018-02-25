5f6d5e063d4f99749bbea253c8e1cb864b0c29b9
Veronika Tolpeeva <osty1n@gmail.com>
2018-02-25 17:34:52 +0300

    refactor solution to pigLatin algorithm

--- pigLatin.js
+++ pigLatin.js
@@ -1,6 +1,8 @@
 function translatePigLatin(str) {
   const firstVowel = str.search(/[aeoui]/);
 
-  return (firstVowel === 0) ? str + 'way' : str.substr(firstVowel) + str.substr(0, firstVowel) + 'ay';
+  return firstVowel === 0
+    ? str + 'way'
+    : str.substr(firstVowel) + str.substr(0, firstVowel) + 'ay';
 }
 console.log(translatePigLatin('consonant'));
