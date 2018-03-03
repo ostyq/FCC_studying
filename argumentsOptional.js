540d2c99263aa0cd7987321506c7efe514f4168a
Veronika Tolpeeva <osty1n@gmail.com>
2018-03-03 21:42:56 +0300

    refactor solution to Arguments Optional algorithm

--- argumentsOptional.js
+++ argumentsOptional.js
@@ -1,33 +1,13 @@
 function addTogether() {
-  const goodNumber = num => Boolean(typeof num === 'number');
-  if (
-    arguments.length === 2 &&
-    goodNumber(arguments[0]) &&
-    goodNumber(arguments[1])
-  ) {
-    return arguments[0] + arguments[1];
-  } else if (arguments.length === 1 && goodNumber(arguments[0])) {
-    return (addSecond = secondArg =>
-      goodNumber(secondArg) ? secondArg + arguments[0] : undefined);
-  } else {
+  const args = Array.from(arguments);
+  if (args.some(num => typeof num !== 'number')) {
     return undefined;
+  } else if (args.length > 1) {
+    return args.reduce((num, acc) => acc + num);
+  } else {
+    return (addSecond = a =>
+      typeof a === 'number' ? a + args[0] : undefined);
   }
 }
 
-console.log(addTogether(2)('a'));
-
-// function addTogether() {
-//     if (
-//       arguments.length === 2 &&
-//       typeof arguments[0] === 'number' &&
-//       typeof arguments[1] === 'number'
-//     ) {
-//       return arguments[0] + arguments[1];
-//     } else if (arguments.length === 1 && typeof arguments[0] === 'number') {
-//       return (addSecond = a => typeof a === 'number'? (a + arguments[0]) : undefined);
-//     } else {
-//       return undefined;
-//     }
-//   }
-
-//   addTogether(2,3);
+console.log(addTogether(2, 3));
