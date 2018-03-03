ddcdc78a6076c1a89084526657f096e0f9ed4e6e
Veronika Tolpeeva <osty1n@gmail.com>
2018-03-03 16:28:04 +0300

    refactor solution to Finders Keepers algorithm

--- findersKeepers.js
+++ findersKeepers.js
@@ -1,7 +1,10 @@
-
 function findElement(arr, func) {
-    const answersArr = arr.filter((element) => func(element));
-    return answersArr[0];
-  }
-  
-  console.log(findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; }));  
+  const answersArr = arr.filter(func);
+  return answersArr[0];
+}
+
+console.log(
+  findElement([1, 2, 3, 4], function(num) {
+    return num % 2 === 0;
+  })
+);
