de9c7bff0eb59f9e7571e4d0ddfeebfe40de2358
Veronika Tolpeeva <osty1n@gmail.com>
2018-03-18 14:01:41 +0300

    add solution to make a person algorithm

---
+++ Advanced algorithm challenges/makeAPerson.js
@@ -0,0 +1,21 @@
+const Person = function(firstAndLast) {
+  let fullName;
+
+  this.setFullName = (full) => {
+    fullName = full.split(' ');
+  };
+  this.setFirstName = (first) => {
+    fullName[0] = first;
+  };
+  this.setLastName = (last) => {
+    fullName[1] = last;
+  };
+
+  this.setFullName(firstAndLast);
+
+  this.getFullName = () => fullName.join(' ');
+  this.getFirstName = () => fullName[0];
+  this.getLastName = () => fullName[1];
+};
+const bob = new Person('Bob Ross');
+console.log(bob.getFullName());
