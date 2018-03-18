3d2c04783e83d93066377bcc9c600a204c72d969
Veronika Tolpeeva <osty1n@gmail.com>
2018-03-18 13:16:30 +0300

    add solution to Inventory Update algorithm

---
+++ Advanced algorithm challenges/inventoryUpdate.js
@@ -0,0 +1,29 @@
+function updateInventory(arr1, arr2) {
+  const itemNames = arr1.map(i => i[1]);
+
+  arr2.forEach((item) => {
+    const currentItemIndex = itemNames.indexOf(item[1]);
+    if (currentItemIndex !== -1) {
+      arr1[currentItemIndex][0] += item[0];
+    } else {
+      arr1.push(item);
+    }
+  });
+  return arr1.sort((a, b) => a[1] > b[1]);
+}
+
+const curInv = [
+  [21, 'Bowling Ball'],
+  [2, 'Dirty Sock'],
+  [1, 'Hair Pin'],
+  [5, 'Microphone'],
+];
+
+const newInv = [
+  [2, 'Hair Pin'],
+  [3, 'Half-Eaten Apple'],
+  [67, 'Bowling Ball'],
+  [7, 'Toothpaste'],
+];
+
+console.log(updateInventory(curInv, newInv));
